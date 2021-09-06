5f27816e60f2ee28e7481094ad7e6802cd5e244e
terakawaakari <terakawaakari0701@gmail.com>
2021-09-06 17:25:05 +0900

    [Add] LoginParamController PathVariable

--- src/main/java/com/example/demo/login/LoginParamController.java
+++ src/main/java/com/example/demo/login/LoginParamController.java
@@ -3,6 +3,7 @@ package com.example.demo.login;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class LoginParamController {
@@ -14,5 +15,16 @@ public class LoginParamController {
 			return "ログインに失敗しました";
 		}
 		return "ログインに成功しました(ID：" + id + "、PASS：" + pass + ")";
+	}
+	
+	@RequestMapping({"/loginParam2", "/loginParam/{id}", "/loginParam2/{id}/{pass}"})
+	public String loginParam2(@PathVariable(required = false) String id, @PathVariable(required = false) String pass) {
+		if (id == null || pass == null) {
+			return "ログイン情報を入力してください";
+		}
+		if (id.equals("") || pass.equals("")) {
+			return "ログインに失敗しました";
+		}
+		return "ログインに成功しました(ID：" + id + "、PASS：" + pass +")";
 	}
 }
