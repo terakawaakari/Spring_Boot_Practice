3056a67a846c2ce9136b2afd513cbe449c221d2f
terakawaakari <terakawaakari0701@gmail.com>
2021-09-07 22:48:18 +0900

    [Add] ThymeleafController

---
+++ src/main/java/com/example/demo/thymeleaf/ThymeleafController.java
@@ -0,0 +1,14 @@
+package com.example.demo.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ThymeleafController {
+	@RequestMapping("/thymeleaf")
+	public ModelAndView thmeleaf(ModelAndView mav) {
+		mav.setViewName("thymeleaf/howto");
+		return mav;
+	}
+}
